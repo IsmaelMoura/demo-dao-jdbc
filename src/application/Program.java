4abcaf3baf6139e80ec663bfd1680b88d206fdeb
Ismael Moura <ismaeldesousa2014@outlook.com>
2023-02-24 12:55:51 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,16 @@
 package application;
 
 import model.entities.Deparment;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
         Deparment deparment = new Deparment(1, "books");
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, deparment);
 
         System.out.println(deparment);
+        System.out.println(seller);
     }
 }
